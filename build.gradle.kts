68835f439061dda05535d70cafc53e0369443de0
Nigel Gaskin <nigel.gaskin.work@gmail.com>
2026-03-05 21:17:09 +1030

    Revert "Reapply "chore: bump version to 9.2.0""
    This reverts commit 9c6e56f58431073dc1ee66fd7bd9a878d8599239.

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,7 @@ configure<org.owasp.dependencycheck.gradle.extension.DependencyCheckExtension> {
     analyzers.assemblyEnabled = false
 }
 
-version = "9.2.0"
+version = "9.1.1"
 group = "com.gaskony"
 
 ignitionModule {
